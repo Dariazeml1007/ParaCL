4f8032e134d80bd9e2bd17586b30ff47fb2b31dc
MatveyDmitryevich2 <kazancevmd0603@gmail.com>
2026-03-20 23:31:26 +0300

    fix: error with tabs in error_print. by @EvGeniy0505

--- src/error/source/error_print.cpp
+++ src/error/source/error_print.cpp
@@ -85,7 +85,7 @@ void PrintDiagnostic(const Diagnostic& diagnostic, const std::string& filename)
         }
     }
 
-    for (std::string_view add_message : diagnostic.add_message)
+    for (const std::string& add_message : diagnostic.add_message)
     {
         std::cout << "  = " << add_message << '\n';
     }
